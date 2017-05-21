d9d791b8d91e7f361d1f8f60f6055504174116ef
Alexandra Marisa Cota <alexandracota@Alexandras-MacBook-Air.local>
2017-05-21 14:00:18 -0700

    version-2

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-function start() {
 
 $(document).ready(function() {
 	$(".wrapper").hide();
-	$(".btn").on("click", function() {
-		$(".wrapper").show();
+
 	});
-start();
+
+$(".btn").click(function() {
+	$(".wrapper").show();
+
 });
 
+//Timer count set to 30.
+var count = 30;
+
+var counter = setInterval (timer, 1000);
+//Function to count down and clear when finished.
+function timer() {
+	count = count--;
+	if (count <= 0) {
+		clearInterval (counter);
+		return;
+	}
+}
+
+timer();
+
+$("#id").html(timer);
+
+// setTimeout(thirtySeconds, 1000 * 30);
+
+// function thirtySeconds() {
+// 	$(".timer").append("")
+// }
+
+// function timeUp() {
+// 	console.log("done");
+// 	$(".timer").append("<h3>Time's up!</h3>");
+// 	console.log("time is up");
+// };
+
+// timeUp();
 
 
-};
+
+// });
+
 
 
 

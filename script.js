581f37b057446be6d0f542b09b6c6912cc4e0796
Alexandra Marisa Cota <alexandracota@Alexandras-Air.att.net>
2017-05-21 21:27:05 -0700

    version-3

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-
+//Hides questions at start of game.
 $(document).ready(function() {
-	$(".wrapper").hide();
+	$(".question").hide();
+	$(".answers").hide();
+
+	
+//Displays questions when start button is clicked.
+$(".start").click(function() {
+	$(".question").show();
+	$(".answers").show();
 
 	});
 
-$(".btn").click(function() {
-	$(".wrapper").show();
-
 });
+
 
 //Timer count set to 30.
 var count = 30;
 
-var counter = setInterval (timer, 1000);
+//Counter to count the seconds.
+var counter = setInterval(timer, 1000);
+
 //Function to count down and clear when finished.
 function timer() {
-	count = count--;
-	if (count <= 0) {
-		clearInterval (counter);
-		return;
-	}
-}
+count = count--;
+if (count <= 0) {
+	clearInterval(counter);
+	return;
+	};
+};
 
-timer();
+// //Call the timer function.
+// timer();
+// setInterval();
+// console.log(count);
 
-$("#id").html(timer);
+// //Display live timer on the document.
+// $("#timer").html(count + " seconds");
 
-// setTimeout(thirtySeconds, 1000 * 30);
+
+
+//
+//setTimeout(thirtySeconds, 1000 * 30);
+
+
 
 // function thirtySeconds() {
 // 	$(".timer").append("")

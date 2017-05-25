204370de92904b82b79e07a6942967d44cc74fed
Alexandra Marisa Cota <alexandracota@Alexandras-Air.att.net>
2017-05-24 23:10:59 -0700

    final version

--- script.js
+++ script.js
@@ -2,15 +2,25 @@
 $(document).ready(function() {
 	$(".question").hide();
 	$(".answers").hide();
+	$(".submit").hide();
 
 
 	//Displays questions when start button is clicked.
 	$(".start").click(function() {
 		$(".question").show();
 		$(".answers").show();
+		$(".submit").show();
 
 		//Set Timeout.
-		var timeOut = function() {	
+		var timeOut = function() {
+			question1();
+			question2();
+			question3();
+			question4();
+			question5();
+			question6();
+			question7();
+
 		};
 		//Time out after 30 seconds.
 		setTimeout(timeOut, 1000 * 30);
@@ -24,111 +34,26 @@ $(document).ready(function() {
 			count--;
 			if (count <= 0) {
 				clearInterval(counter);
-				console.log("time's up!");
-				$("#timer").html("Correct: " + correctAnswerCounter + " Incorrect: " + incorrectAnswerCounter);
+				$("#timer").html("Correct: " + correctAnswerCounter + " Incorrect: " + incorrectAnswerCounter + " Unanswered: " + unansweredCounter);
 			}
 			else {
 				$("#timer").html("Time remaining: 00:" + count + " seconds");
 			}
-		};
+		$(".submit").on("click", function submit() {
+		clearInterval(counter);
+		$("#timer").html("Correct: " + correctAnswerCounter + " Incorrect: " + incorrectAnswerCounter + " Unanswered: " + unansweredCounter);
+		$(".question").hide();
+		$(".answers").hide();
+		$(".submit").hide();
+
+		});
+	
+	};
+	
+
 
 	});
 
-});
-
-//Create answer counter.
-var answerCounter = 0;
-
-	//If group 1 has an answer selected, add to answer counter.
-	function question1() { 
-		if ($('input[name="radio-1"]').is(':checked')) {
-			answerCounter++;
-		} 
-		else if (!$('input[name="radio-1"]').is(':checked')) {
-			unansweredCounter++;
-		};
-
-	question1();
-	console.log(answerCounter);
-	};
-
-	//If group 2 has an answer selected, add to answer counter.
-		function question2() { 
-		if ($('input[name="radio-2"]').is(':checked')) {
-			answerCounter++;
-		} 
-		else if (!$('input[name="radio-2"]').is(':checked')) {
-			unansweredCounter++;
-		};
-
-	question2();
-	console.log(answerCounter);
-	};
-
-	//If group 3 has an answer selected, add to answer counter.
-		function question3() { 
-		if ($('input[name="radio-3"]').is(':checked')) {
-			answerCounter++;
-		} 
-		else if (!$('input[name="radio-3"]').is(':checked')) {
-			unansweredCounter++;
-		};
-
-	question3();
-	console.log(answerCounter);
-	};
-
-	//If group 4 has an answer selected, add to answer counter.
-		function question4() { 
-		if ($('input[name="radio-4"]').is(':checked')) {
-			answerCounter++;
-		} 
-		else if (!$('input[name="radio-4"]').is(':checked')) {
-			unansweredCounter++;
-		};
-
-	question4();
-	console.log(answerCounter);
-	};
-
-	//If group 5 has an answer selected, add to answer counter.
-		function question5() { 
-		if ($('input[name="radio-5"]').is(':checked')) {
-			answerCounter++;
-		} 
-		else if (!$('input[name="radio-5"]').is(':checked')) {
-			unansweredCounter++;
-		};
-
-	question5();
-	console.log(answerCounter);
-	};
-
-	//If group 6 has an answer selected, add to answer counter.
-		function question6() { 
-		if ($('input[name="radio-6"]').is(':checked')) {
-			answerCounter++;
-		} 
-		else if (!$('input[name="radio-6"]').is(':checked')) {
-			unansweredCounter++;
-		};
-
-	question6();
-	console.log(answerCounter);
-	};
-
-	//If group 7 has an answer selected, add to answer counter.
-		function question7() { 
-		if ($('input[name="radio-7"]').is(':checked')) {
-			answerCounter++;
-		} 
-		else if (!$('input[name="radio-7"]').is(':checked')) {
-			unansweredCounter++;
-		};
-
-	question7();
-	console.log(answerCounter);
-	};
 
 
 //How many answered correctly.
@@ -136,20 +61,110 @@ var correctAnswerCounter = 0;
 //How many answered incorrectly.
 var incorrectAnswerCounter = 0;
 //How many unanswered.
-var unansweredCounter = 0;
+var unansweredCounter = 7;
 
-//If answerCounter is equal to 7, stop the timer and end game.
-if (answerCounter === 7) {
-	clearInterval(timer);
-	$(".question").hide();
-	$(".answers").hide();
-	$("#timer").html("Correct: " + correctAnswerCounter + " Incorrect: " + incorrectAnswerCounter);
+	//If group 1 has an answer selected, add to answer counter.
+	function question1() { 
+		var answer = $('input[name="radio-1"]:checked').val(); 
+		if (answer === "correct") {
+			correctAnswerCounter++;
+			unansweredCounter--;
+		} 
+		else if (answer === "incorrect") {
+			incorrectAnswerCounter++;
+			unansweredCounter--;
+		}
 
-};
+		console.log(answer);
+	};
+	
+
+	//If group 2 has an answer selected, add to answer counter.
+	function question2() { 
+		var answer = $('input[name="radio-2"]:checked').val(); 
+		if (answer === "correct") {
+			correctAnswerCounter++;
+			unansweredCounter--;
+		} 
+		else if (answer === "incorrect") {
+			incorrectAnswerCounter++;
+			unansweredCounter--;
+		}
+	};
+	
+
+
+	//If group 3 has an answer selected, add to answer counter.
+	function question3() { 
+		var answer = $('input[name="radio-3"]:checked').val(); 
+		if (answer === "correct") {
+			correctAnswerCounter++;
+			unansweredCounter--;
+		} 
+		else if (answer === "incorrect") {
+			incorrectAnswerCounter++;
+			unansweredCounter--;
+		}
+	};
+	
+
+	//If group 4 has an answer selected, add to answer counter.
+	function question4() { 
+		var answer = $('input[name="radio-4"]:checked').val(); 
+		if (answer === "correct") {
+			correctAnswerCounter++;
+			unansweredCounter--;
+		} 
+		else if (answer === "incorrect") {
+			incorrectAnswerCounter++;
+			unansweredCounter--;
+		}
+	};
+
+	
+	//If group 5 has an answer selected, add to answer counter.
+	function question5() { 
+		var answer = $('input[name="radio-5"]:checked').val(); 
+		if (answer === "correct") {
+			correctAnswerCounter++;
+			unansweredCounter--;
+		} 
+		else if (answer === "incorrect") {
+			incorrectAnswerCounter++;
+			unansweredCounter--;
+		}
+	};
+
+
+	//If group 6 has an answer selected, add to answer counter.
+	function question6() { 
+		var answer = $('input[name="radio-6"]:checked').val(); 
+		if (answer === "correct") {
+			correctAnswerCounter++;
+			unansweredCounter--;
+		} 
+		else if (answer === "incorrect") {
+			incorrectAnswerCounter++;
+			unansweredCounter--;
+		}
+	};
+
+	//If group 7 has an answer selected, add to answer counter.
+	function question7() { 
+		var answer = $('input[name="radio-7"]:checked').val(); 
+		if (answer === "correct") {
+			correctAnswerCounter++;
+			unansweredCounter--;
+		} 
+		else if (answer === "incorrect") {
+			incorrectAnswerCounter++;
+			unansweredCounter--;
+		}
+	};
 
 
 
-
+});
 
 
 
